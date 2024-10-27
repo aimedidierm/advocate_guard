bc0b0dbae8614a0834393c1825a0c097c743925b
Aime Didier MUGISHA <aimedidiermugisha@gmail.com>
2024-10-27 16:01:02 +0200

    seeder

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Enums\UserRole;
+use App\Http\Requests\LoginRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $request)
+    {
+        $email = $request->input('email');
+        $password = $request->input('password');
+        $user = User::where('email', $email)->first();
+        if ($user != null) {
+            $passwordMatch = Hash::check($password, $user->password);
+            if ($passwordMatch) {
+
+                Auth::login($user);
+                if (Auth::user()->role == UserRole::ADMIN->value) {
+                    return redirect("/admin");
+                } elseif (Auth::user()->role == UserRole::COMMUNITY->value) {
+                    return redirect("/community");
+                } else {
+                    return redirect("/child");
+                }
+            } else {
+                return redirect(route('login'))->withErrors(['msg' => 'Incorect password']);
+            }
+        } else {
+            return redirect(route('login'))->withErrors(['msg' => 'Incorect email and password']);
+        }
+    }
+
+    public function logout()
+    {
+        if (Auth::check()) {
+            Auth::logout();
+            return redirect(route("login"));
+        } else {
+            return back();
+        }
+    }
+}
