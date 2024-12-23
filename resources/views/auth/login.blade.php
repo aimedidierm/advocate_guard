9148d1a5631e56dd924e83dc1f481a5828cd7c7a
serge <sergohakiza@gmail.com>
2024-12-23 08:49:46 +0200

    my local changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -22,6 +22,7 @@
 
 <body>
     <x-landing-page-navbar />
+    
     <section class="bg-gray-50 dark:bg-gray-900">
         <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
             <div
